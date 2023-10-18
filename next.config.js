927f866a9136c2c03ebca738d4e2a47fdd8fe78a
Soroosh Jaberi <93154421+sorooshjaberi@users.noreply.github.com>
2023-10-18 06:51:56 +0330

    next config for ssr tailwind

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   env : {
     NEXT_PUBLIC_APP_TITLE : "Musix"
-  }
+  },
+  experimental: {
+    optimizeCss: true,
+  },
 }
 
 module.exports = nextConfig
